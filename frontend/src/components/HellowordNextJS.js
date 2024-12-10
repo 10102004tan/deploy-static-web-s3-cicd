63817d4ad89d285b6df9f3bdf7a23ae508ccb9eb
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-12-10 23:46:55 +0700

    update ui

---
+++ frontend/src/components/HellowordNextJS.js
@@ -0,0 +1,22 @@
+export default function HellowordNextJS() {
+    return (
+        <div class="pyramid">
+            <div class="glow"></div>
+            <div>
+                <span style={{
+                    "--i":0
+                }}></span>
+                <span style={{
+                    "--i":1
+                }}></span>
+                <span style={{
+                    "--i":2
+                }}></span>
+                <span style={{
+                    "--i":3
+                }}></span>
+                
+            </div>
+        </div>
+    )
+}
